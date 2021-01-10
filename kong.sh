20feb1a7932ad207b00f0049f3f3771bcc352760
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2021-01-10 13:35:34 +0100

    Removed kong script

--- kong.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm \
-    --link kong-db:kong-database \
-    --net=docker-templates_default \
-    -e "KONG_DATABASE=cassandra" \
-    -e "KONG_CASSANDRA_CONTACT_POINTS=kong-database" \
-    kong kong migrations bootstrap
